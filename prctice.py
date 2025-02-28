70d7aad32ce4811e9c645f1f3880d2e91db99241
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2025-02-28 16:11:29 +0100

    Metaclass

--- prctice.py
+++ prctice.py
@@ -1,15 +1,59 @@
-class Car:
+# # Practicing a simple class
+# class Car:
 
-    def __init__(self,make,model,year):
-        self.make = make
-        self.model = model
-        self.year = year
+#     def __init__(self,make,model,year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
 
-    def start_engine(self):
-        print(f"The {self.make} {self.model} engine is running")
+#     def start_engine(self):
+#         print(f"The {self.make} {self.model} engine is running")
 
 
-car = Car(1990,"honda",2021)
-print (car.start_engine())
+# car = Car(1990,"honda",2021)
+# print (car.start_engine())
+
+
+# class BankAccount:
+#     def __init__(self, initial_balance):
+#         self._balance = initial_balance
+
+#     @property
+#     def balance(self):
+#         return self._balance
+
+#     def deposit(self, amount):
+#         self._balance += amount
+
+#     def withdraw(self, amount):
+#         if self._balance >= amount:
+#             self._balance -= amount
+#         else:
+#             print("Insufficient funds.")
+
+# account = BankAccount(1000)
+# print(account.balance)  # Output: 1000
+# account.deposit(500)
+# print(account.balance)  # Output: 1500
+
+# Creating Metaclass
+
+class Car(type):
+    def __new__(cls,name,base,attr):
+     
+        if 'name' not in attr:
+                raise Exception(f"if 'name' attribute not found in{name} not found")
+        else:
+            return super().__new__(cls,name,base,attr)
+        
+
+class Manual(metaclass=Car):
+     name = "honda"
+
+car = Manual()
+print(car.name)
+
+            
+            
 
 
